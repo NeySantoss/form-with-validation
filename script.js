7f9e939e89eb565df66af46365110508f277266e
Neyyss <neyysss@hotmail.com>
2023-10-08 07:44:35 -0300

    form with validation

---
+++ script.js
@@ -0,0 +1,69 @@
+const form   = document.getElementById('form');
+const campos = document.querySelectorAll('.required');
+const spans  = document.querySelectorAll ('.span-required')
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    nameValidate();
+    emailValidate();
+    passowordValidade();
+    comparePassword();
+
+});
+
+function setError(index){
+    campos[index].style.border = '2px solid #e63636';
+    spans[index].style.display = 'block';
+}
+
+function removeError(index) {
+    campos[index].style.border = '';
+    spans[index].style.display = '';
+}
+
+
+function nameValidate(){
+    if(campos[0].value.length < 3)
+    {
+        setError(0);
+    }
+    else
+    {
+        removeError(0);
+    }
+}
+
+function emailValidate(){
+    if(!emailRegex.test(campos[1].value))
+    {
+        setError(1);
+    }
+    else
+    {
+        removeError(1);
+    }
+}
+
+function passowordValidade(){
+    if(campos[2].value.length < 8)
+    {
+        setError(2);
+    }
+    else
+    {
+        removeError(2);
+        comparePassword();
+    }     
+}
+
+function comparePassword(){
+    if(campos[2].value == campos[3].value && campos[3].value.length >= 8)
+    {
+        removeError(3);
+    }
+    else{
+        setError(3);
+    }
+}
+
